13328360d250714256feadcabf1f212c0475d9f4
dani <dani.indreika@gmail.com>
2022-03-18 13:54:44 +0100

    Work in progress

--- chain.js
+++ chain.js
@@ -3,23 +3,27 @@ import Block from "./block.js";
 
 // Klass för blockkedjan
 class BlockChain {
-    constructor() {
-        try {
-            this.chain = JSON.parse(FS.readFileSync("chain"));
-        }
-        catch {
-            this.chain = [];
-        }
+    
+    get chain() {
+      if(FS.existsSync('./chain')){
+        return JSON.parse(FS.readFileSync("chain"));
+      }
+      return [];
+      
     }
 
-    //
+    set chain(value){
+        FS.writeFileSync("chain", JSON.stringify(value), null, 6);
+    }
+    
+
     validate() {
-        // Bakåt
+
         for(let i = 1; i < this.chain.length; i++) {
             let prevBlock = this.chain[i-1];
             let currentBlock = this.chain[i];
             
-            if (!currentBlock.prevHash == prevBlock.blockHash) {
+            if (currentBlock.prevHash != prevBlock.blockHash) {
                 return false;
             }
         }
@@ -30,10 +34,10 @@ class BlockChain {
     addBlock(data) {
         let blockId = this.chain.length;
         let previousHash = this.chain.length !== 0 ? this.chain[this.chain.length - 1].blockHash : "";
-        let block = new Block(blockId, previousHash, data, 2);
+        let block = new Block(blockId, previousHash, data, 1);
 
-        this.chain.push(block);
-        FS.writeFileSync("chain", JSON.stringify(this.chain), null, 6);
+        
+        this.chain = [...this.chain, block];
     }
 
 }
